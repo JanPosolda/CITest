f99ad3203bbcb4d5870e8b098422bcdc2e02521a
Jan Posolda <posolda@shean.cz>
2017-01-26 11:20:09 +0100

    init

---
+++ Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jan Posolda
+ * Date: 26.01.2017
+ * Time: 11:09
+ */
+
+namespace JanPosolda;
+
+
+class Test
+{
+
+	public function getOneNumber()
+	{
+		return 1;
+	}
+}
