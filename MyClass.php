4e20883de2b5033468d4da42d9197dc19e24f3ed
Jan Posolda <posolda@shean.cz>
2017-01-26 11:28:33 +0100

    testy

---
+++ MyClass.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jan Posolda
+ * Date: 26.01.2017
+ * Time: 11:09
+ */
+
+namespace JanPosolda;
+
+
+class MyClass
+{
+
+	public function getOneNumber()
+	{
+		return 1;
+	}
+}
