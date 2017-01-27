d2b2577523def701a8b44807a694b24093f501f5
Jan Posolda <posolda@shean.cz>
2017-01-27 13:04:41 +0100

    deploy 7

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-ssh posolda@31.31.74.179 -p 2222
-
 echo 'Updating master'
-git pull origin master
-
+ssh -i posolda posolda@31.31.74.179 -p 2222 "cd /var/www/vhosts/shdev.cz/testci.shdev.cz && git pull origin master"
 echo 'Done!'
